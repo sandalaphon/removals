ecda900f1227aabb89b986499abc3407e27073d5
Richard Page-Croft <rpcroft@gmail.com>
2017-07-05 10:59:35 +0100

    minor bug fix

--- react/client/components/home/Home.jsx
+++ react/client/components/home/Home.jsx
@@ -4,8 +4,6 @@ import Geocoder from '../../models/geocoder'
 class Home extends React.Component {
 
   render(){
-    var newGeocoder =new Geocoder()
-    newGeocoder.getLatLng('EH74HU')
     return(
       <p>We are Home</p>
       )
