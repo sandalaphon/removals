0974a61a2fa176bb0db6f037b5055a8c7fb13eb7
jozsef meszaros <joseph.meszaros89@yahoo.co.uk>
2017-08-23 11:36:45 +0100

    make table div only

--- react/client/components/BranchesInfo.jsx
+++ react/client/components/BranchesInfo.jsx
@@ -24,62 +24,6 @@ class BranchesInfo extends React.Component{
     alert('get photo')
   }
  
-  // getEmployeeBranch(branch_id){
-  //   var branchCode 
-  //   this.props.all_branches.forEach((branch)=>{
-  //       if(branch.id==branch_id) branchCode=branch.branch_code
-  //   })
-  //   return branchCode
-  // }
-
-  getEmployeeTable(branchId){
-    if(!this.props.all_employees) return
-    if(!this.props.all_branches) return
-      var pictureOrButton
-      var pictureSize = {
-        height: 'auto', 
-        width: 'auto', 
-        maxWidth: '70px', 
-        maxHeight: '70px',
-      }
-
-    return this.props.all_employees.map((employee, index)=>{
-      if (employee.branch_id==branchId){
-        pictureOrButton = employee.photoUrl ? <img style={pictureSize} src={employee.photoUrl}></img> : <button id={employee.id} onClick={this.handleImageClick.bind(this)}>Upload Picture</button>
-        
-        return(
-         
-          <tr  key={employee.id}>
-            
-            <td>{pictureOrButton}</td>
-            <td>{employee.name}</td>
-            <td>{employee.email}</td>
-            <td>{employee.telephone}</td>
-          </tr>)
-      }  
-    })
-
-  }
-
-
-
-  branchlist() {
-    if(!this.props.all_branches) return
-
-    return this.props.all_branches.map((branch)=>{
-      return (
-        <div>
-          <button className="branch-accordion" id={branch.id}>{branch.name}</button>
-          <div className="employee-panel">
-            {this.getEmployeeTable(branch.id)}
-          </div>
-        </div>
-
-      )
-    })
-      
-  }
-
   accordion(){
     var acc = document.getElementsByClassName("branch-accordion");
     var i;
@@ -96,24 +40,74 @@ class BranchesInfo extends React.Component{
       }
     }
   }
+  // getEmployeeBranch(branch_id){
+  //   var branchCode 
+  //   this.props.all_branches.forEach((branch)=>{
+  //       if(branch.id==branch_id) branchCode=branch.branch_code
+  //   })
+  //   return branchCode
+  // }
+
+  getEmployeeTable(branchId){
+    if(!this.props.all_employees) return
+    if(!this.props.all_branches) return
+      ////////////////////////////////////////////
+      var pictureOrButton
+      var pictureSize = {
+        height: 'auto', 
+        width: 'auto', 
+        maxWidth: '70px', 
+        maxHeight: '70px',
+      }
+
+    return this.props.all_employees.map((employee, index)=>{
+      if (employee.branch_id==branchId){
+        pictureOrButton = employee.photoUrl ? <img style={pictureSize} src={employee.photoUrl}></img> : <button id={employee.id} onClick={this.handleImageClick.bind(this)}>Upload Picture</button>
+        return(
+          <div  className="employee-row" key={employee.id}>
+            <div className="employee-cell" >{pictureOrButton}</div>
+            <div className="employee-cell">{employee.name}</div>
+            <div className="employee-cell">{employee.email}</div>
+            <div className="employee-cell">{employee.telephone}</div>
+          </div>)
+      }  
+    })
+
+  }
+
+
+
+  branchlist() {
+    if(!this.props.all_branches) return
+      //////////////////////////////////
+    return this.props.all_branches.map((branch)=>{
+      return (
+        <div key={branch.id} className="branch-row">
+          <button className="branch-accordion" id={branch.id}>{branch.name}</button>
+          <div className="employee-panel">
+            {this.getEmployeeTable(branch.id)}
+          </div>
+        </div>
+
+      )
+    })
+      
+  }
+
  
   render(){
     this.accordion()
     return(
       <div className= 'branch-info-table' >
-        <table >
-        <tbody>
-          <tr>
-            <th>Photo</th>
-            <th>Name</th>
-            <th>email</th>
-            <th>Phone No.</th>
-          </tr>
+          <div className="branch-header-row">
+            <div className="branch-header-cell">Photo</div>
+            <div className="branch-header-cell">Name</div>
+            <div className="branch-header-cell">email</div>
+            <div className="branch-header-cell">Phone No.</div>
+          </div>
           
            {this.branchlist.call(this)}
 
-        </tbody>
-        </table>
       </div>
       )
   }
