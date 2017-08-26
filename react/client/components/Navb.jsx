13abbf3a851b0c3451092ef5065fa16bda9158a9
jozsef meszaros <joseph.meszaros89@yahoo.co.uk>
2017-08-26 15:27:16 +0100

    after jsinteract

--- react/client/components/Navb.jsx
+++ react/client/components/Navb.jsx
@@ -2,7 +2,37 @@ import React from 'react'
 import {Link} from 'react-router'
 import {mapObjectInstances} from '../models/mapObject'
 import BranchesInfo from './BranchesInfo.jsx'
+import interact from 'interact.js'
 // import { LinkContainer } from 'react-router-bootstrap';
+interact('.resize-drag')
+  .draggable({
+    onmove: window.dragMoveListener
+  })
+  .resizable({
+    preserveAspectRatio: false,
+    edges: { left: true, right: true, bottom: true, top: true }
+  })
+  .on('resizemove', function (event) {
+    var target = event.target,
+        x = (parseFloat(target.getAttribute('data-x')) || 0),
+        y = (parseFloat(target.getAttribute('data-y')) || 0);
+
+    // update the element's style
+    target.style.width  = event.rect.width + 'px';
+    target.style.height = event.rect.height + 'px';
+
+    // translate when resizing from top or left edges
+    x += event.deltaRect.left;
+    y += event.deltaRect.top;
+
+    target.style.webkitTransform = target.style.transform =
+        'translate(' + x + 'px,' + y + 'px)';
+
+    target.setAttribute('data-x', x);
+    target.setAttribute('data-y', y);
+    //target.textContent = Math.round(event.rect.width) + '×' + Math.round(event.rect.height);
+  });
+
 
 class Navb extends React.Component {
   signOut(e){
@@ -43,6 +73,11 @@ class Navb extends React.Component {
 
    }
 
+
+   
+
+
+
  render(){
 
   return(
@@ -50,8 +85,8 @@ class Navb extends React.Component {
     <nav  className="nav-bar">
 
       <button id="button-branch-list-nav" onClick={this.toggleBranchList.bind(this)}>Branch List</button>
-      
-      <div class="dropdown-content" id="myDropdown">
+
+      <div className="dropdown-content resize-drag" id="myDropdown">
             <BranchesInfo/>
       </div>
 
